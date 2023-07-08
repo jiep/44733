1e6f5b45265c7c4dac6bb9aa84c1e05d7b46fc13
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-08 18:39:52 +0000

    Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav class="bg-yellow-500 py-4">
-    <div class="container mx-auto flex items-center justify-center">
-      <div class="flex items-center">
-        <img src="@/assets/logo.png" alt="Logo" class="h-12 w-12">
-        <span class="text-white text-2xl font-semibold">44733</span>
+
+  <header class="bg-yellow-500 flex h-16 items-center px-4">
+      <div class="flex flex-1 items-center justify-center min-h-full">
+        <nav>
+          <div class="flex items-center">
+            <img src="@/assets/logo.png" alt="Logo" class="h-12 w-12">
+            <span class="text-white text-2xl font-semibold">44733</span>
+          </div>
+        </nav>
       </div>
+  </header>
+  <!-- <nav class="bg-yellow-500 py-4">
+    <div class="container mx-auto flex items-center justify-center">
+      
     </div>
-  </nav>
+  </nav> -->
 </template>
