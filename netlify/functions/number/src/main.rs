d62fceb46d83655451f30ba0b9d2413be1b120f5
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-09 13:23:07 +0000

    Fix error with url

--- netlify/functions/number/src/main.rs
+++ netlify/functions/number/src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<(), Error> {
 pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
     let _path = event.path.unwrap();
 
-    let url: &str = "https://www.loteriasyapuestas.es/servicios/premioDecimoWeb?idsorteo=1186309102";
+    let url: &str = "https://www.loteriasyapuestas.es/servicios/premioDecimoWeb?idsorteo=1222809102";
 
     let number: u32 = 44733;
 
