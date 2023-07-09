61a0ac6036a0176821d1b041de8161dd84df8b72
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-09 14:01:27 +0000

    Add suspense fallback

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,12 @@ import NavBar from './components/NavBar.vue'
   <section class="h-full">
     <NavBar/>
     <Suspense class="h-full bg-yellow-50">
-      <RouterView class="h-full bg-yellow-50" />
+      <template #default>
+        <RouterView class="h-full bg-yellow-50" />
+      </template>
+      <template #fallback>
+        Loading...
+      </template>
     </Suspense>
   </section>
 </template>
