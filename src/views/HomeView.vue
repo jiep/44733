f13eb0a843b011bfa8558a0966f8520e2b30050f
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-10 18:35:18 +0000

    Add reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,29 +2,34 @@
   import { reactive, ref } from 'vue'
 
   import LocationList from '@/components/LocationList.vue'
-  import Tabs from '@/components/Tabs.vue'
   import { Location }from '@/model/Location'
 
   const number = ref(44733)
 
+  const locations = reactive(new Array<Location>);
+  
   const response = await fetch(`.netlify/functions/number?number=${number.value}`)
-
   const data = await response.json()
 
   let items: Array<Location> = data.locations;
   items = items.map(x => new Location(x.name, x.address, x.city, x.province, x.series))
                .sort((a: Location, b:Location) => b.series.length - a.series.length)
 
+  locations.push(...items)
+
   async function onClick() {
-    // console.log(number.value);
+    console.log(number.value);
     
-    // const response = await fetch(`.netlify/functions/number?number=${number.value.toString().padStart(5, '0')}`)
-    // const data = await response.json()
+    const response = await fetch(`.netlify/functions/number?number=${number.value.toString().padStart(5, '0')}`)
+    const data = await response.json()
 
-    // const d: Array<Location> = data.locations;
+    const d: Array<Location> = data.locations;
 
-    // items = d.map(x => new Location(x.name, x.address, x.city, x.province, x.series))
-    //          .sort((a: Location, b:Location) => b.series.length - a.series.length)
+    items = d.map(x => new Location(x.name, x.address, x.city, x.province, x.series))
+             .sort((a: Location, b:Location) => b.series.length - a.series.length)
+
+    locations.length = 0;
+    locations.push(...items)
   }
 
 </script>
