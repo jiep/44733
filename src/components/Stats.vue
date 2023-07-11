479426103d4d31f66f09b9339c1e640d271a6ac1
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-11 17:14:58 +0000

    Fix label

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -15,7 +15,7 @@
                 <div class="bg-white p-5">
                     <div class="sm:flex sm:items-start">
                         <div class="text-center sm:mt-0 sm:ml-2 sm:text-left">
-                            <h3 class="text-sm leading-6 font-medium text-gray-400">Localizaciones</h3>
+                            <h3 class="text-sm leading-6 font-medium text-gray-400">Número</h3>
                             <p class="text-3xl font-bold text-black">{{ lottery_number }}</p>
                         </div>
                     </div>
