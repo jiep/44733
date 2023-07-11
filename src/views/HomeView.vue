349e383532f90ac53cf12bf008c6fbd207a5e2ba
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-11 17:34:27 +0000

    Fix button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -51,7 +51,7 @@
         class="px-3 py-3 placeholder-slate-400 text-slate-600 relative bg-white bg-white text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full" 
         v-model="number" 
       />
-      <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" @click="onClick(number)"
+      <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150" type="button" @click="onClick(number)"
       >Buscar</button>
     </div>
     <PrimosAdri @selectedPrimoAdri="onSelectedPrimoAdri" />
