157030f397d72f456792a838b1cd551eb0349f3e
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-12 17:50:08 +0000

    Fix error

--- src/components/LocationList.vue
+++ src/components/LocationList.vue
@@ -13,11 +13,10 @@ defineProps<{
 
 <template>
     <section class="flex flex-col h-full overflow-y-auto">
-        <div class="my-1  mx-4 last:mb-4" v-for="item in items" v-if="!isLoading">  
-            <LocationItem :location="item" />
-        </div>
         <template v-if="items.length == 0 && !isLoading">
-            <p class="flex align-middle mx-4 h-full text-gray-400 font-medium">No se encontró ninguna localización para el número solicitado</p>
+            <div class="flex flex-col items-center justify-center h-full">
+                <p class="flex align-middle mx-4 h-full text-gray-400 font-medium">No se encontró ninguna localización para el número solicitado</p>
+            </div>
         </template>
         <template v-if="isLoading">
             <div class="flex flex-col items-center justify-center h-full">
@@ -25,6 +24,9 @@ defineProps<{
                 <p class="flex align-middle mx-4 h-full text-gray-400 font-medium">Cargando localizaciones...</p>
             </div>
         </template>
+        <div class="my-1  mx-4 last:mb-4" v-for="item in items" v-if="!isLoading">  
+            <LocationItem :location="item" />
+        </div>
     </section>
       
 </template>
