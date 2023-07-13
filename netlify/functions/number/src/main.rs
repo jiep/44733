e6ac5d384fda4ffd13ea5eaabb564f465b968ade
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-13 18:28:55 +0000

    Revert commit

--- netlify/functions/number/src/main.rs
+++ netlify/functions/number/src/main.rs
@@ -4,7 +4,6 @@ use http::header::HeaderMap;
 use lambda_runtime::{handler_fn, Context, Error};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
-use std::env;
 use lottery::{finder::lottery::Lottery};
 
 #[tokio::main]
@@ -17,12 +16,7 @@ async fn main() -> Result<(), Error> {
 }
 
 pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
-
-    const DRAW_ID_ENV: &str = "DRAW_ID";
-
-    let draw_id = env::var(DRAW_ID_ENV).expect("$DRAW_ID is not set");
-
-    let url = format!("https://www.loteriasyapuestas.es/new-geo-web/JsonGenerationServlet/exportPois.txt?drawId={}&number=", draw_id);
+    let url: &str = "https://www.loteriasyapuestas.es/new-geo-web/JsonGenerationServlet/exportPois.txt?drawId=1222809102&number=";
 
     let number: u32 = event
         .query_string_parameters.first("number").unwrap_or("00000").parse::<u32>().unwrap();
