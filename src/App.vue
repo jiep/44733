69c8cfef5826ccef1aa8d1bfcf567cfd08a87927
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-07-15 07:31:24 +0000

    Add sticky navbar

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <NavBar />
-  <section class="flex flex-col min-h-full bg-yellow-50">
+  <NavBar class="sticky top-0 z-50" />
+  <section class="relative flex flex-col min-h-full bg-yellow-50">
     <Suspense>
       <template #default>
         <RouterView />
