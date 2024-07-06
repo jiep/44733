b9b6e820371952021d3cf4c2524a5e7a25c9bba1
José Ignacio Escribano <jiep@users.noreply.github.com>
2024-07-06 16:36:09 +0000

    Format constants

--- netlify/functions/number/src/consts.rs
+++ netlify/functions/number/src/consts.rs
@@ -1,4 +1,4 @@
-pub const DEFAULT_DRAW_ID: u32 = 1222809102;
+pub const DEFAULT_DRAW_ID:          u32  = 1222809102;
 pub const DEFAULT_NUMBER_AS_STRING: &str = "00000";
-pub const QUERYPARAM_NAME: &str = "number";
-pub const DRAW_ID_ENV_VAR_NAME: &str = "DRAW_ID";
+pub const QUERYPARAM_NAME:          &str = "number";
+pub const DRAW_ID_ENV_VAR_NAME:     &str = "DRAW_ID";
