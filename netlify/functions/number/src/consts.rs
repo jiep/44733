9f3b82c63209cb3913e23be2bfddc8a2b2fe656f
José Ignacio Escribano <jiep@users.noreply.github.com>
2024-07-12 15:41:45 +0000

    Remove Rust functions

--- netlify/functions/number/src/consts.rs
+++
@@ -1,4 +0,0 @@
-pub const DEFAULT_DRAW_ID:          u32  = 1259409102;
-pub const DEFAULT_NUMBER_AS_STRING: &str = "00000";
-pub const QUERYPARAM_NAME:          &str = "number";
-pub const DRAW_ID_ENV_VAR_NAME:     &str = "DRAW_ID";
